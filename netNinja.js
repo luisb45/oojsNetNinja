0ba1db003f85c8fd7296123af06c5a922844dcf0
Luis Bustos <luisbustos500@gmail.com>
2022-10-19 10:03:27 -0600

    video 1

---
+++ netNinja.js
@@ -0,0 +1,13 @@
+// #1 - Introduction
+let names = ['ryu', 'crystal', 'mario']
+
+
+names.length // 3
+
+names.sort();
+console.log(names);
+
+console.log(window); //window global object
+
+let name2 = new String('ryu');
+console.log(name2);
