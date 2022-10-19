f715362264d3996c82cf8885ca67c6b35a910b1f
Luis Bustos <luisbustos500@gmail.com>
2022-10-19 12:15:06 -0600

    Lesson 11

--- netNinja.js
+++ netNinja.js
@@ -41,7 +41,7 @@ userOne.age = 25;
 
 
 
-
+/*
 // - classes
 class User {
     constructor(email, name) {
@@ -64,6 +64,16 @@ class User {
     }
 }
 
+//class inheritance
+class Admin extends User{
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email != user.email;
+        })
+    }
+}
+
+var admin = new Admin('shaun@ninjas.com', 'Shaun');
 var userOne = new User('luis@ninjas.com', 'luis');
 var userTwo = new User('yoshi@mariocorp.com', 'Yoshi');
 //'new' keyword creates new empty object{}
@@ -71,8 +81,57 @@ var userTwo = new User('yoshi@mariocorp.com', 'Yoshi');
 //calls constructor method
 
 //'return this' each method for method chaining
-userOne.login().updateScore().updateScore().logout();
+//userOne.login().updateScore().updateScore().logout();
+
+
+var users = [userOne, userTwo, admin];
+
+admin.deleteUser(userOne);
+console.log(users);
+*/
 
 
 
+// Constructor function
+function User(email, name){
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login = function() {
+        console.log(this.email, 'has logged in');
+    }
+}
 
+//adding methods to prototype
+User.prototype.login = function() {
+    this.online = true;
+    console.log(this.email, 'has logged in');
+}
+
+User.prototype.logout = function() {
+    this.online = false;
+    console.log(this.email, 'has logged out');
+}
+
+function Admin(...args){
+    User.apply(this, args);
+    this.role = 'super admin';
+}
+
+//prototype inheritance
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user => {
+        return user.email != u.email;
+    });
+};
+
+
+var userOne = new User('luis@ninjas.com', 'luis');
+var userTwo = new User('yoshi@mariocorp.com', 'Yoshi');
+var admin = new Admin('billy@ninja.com', 'billy');
+
+var users = [userOne, userTwo, admin];
+
+console.log(admin);
