1b0c0628b32b3e60eae454a4fb654155c907a4ce
Luis Bustos <luisbustos500@gmail.com>
2022-10-19 10:46:06 -0600

    lession 3

--- netNinja.js
+++ netNinja.js
@@ -1,7 +1,6 @@
 // #1 - Introduction
-let names = ['ryu', 'crystal', 'mario']
-
-
+/*
+let names = ['ryu', 'crystal', 'mario'];
 names.length // 3
 
 names.sort();
@@ -9,5 +8,32 @@ console.log(names);
 
 console.log(window); //window global object
 
-let name2 = new String('ryu');
+let name2 = new String('ryu'); //string object 
 console.log(name2);
+*/
+
+
+// #2 - Object Literals
+var userOne = {
+    email: 'luis@ninjas.com',
+    name: 'Luis',
+    login() {
+        console.log(this.email, 'has logged in');
+    },
+    logout() {
+        console.log(this.email, 'has logged out')
+    }
+};
+
+console.log(userOne.name);
+
+
+// #3 updating properties
+userOne.name = 'Yoshi'; 
+userOne['name'] = 'mario';
+
+//creating new properties/methods
+userOne.age = 25;
+
+
+//#4 - classes
